105a8203b98cb93983c5bd2735ec5c0d52a0f650
dark <lolingarsg@gmail.com>
2026-06-04 18:45:22 +0300

    added readme + exposed struct

--- Omnihook/MidHook.h
+++ Omnihook/MidHook.h
@@ -1,51 +1,13 @@
 #pragma once
 #include <unordered_map>
+#include <vector>
 #include "IHook.h"
 
 
-#pragma pack(push, 1)
-struct YMMRegister {
-    uint64_t qwords[4];
-};
+#include "Omnihook.h"
 
-
-struct RegisterContext {
-    YMMRegister ymm0;
-    YMMRegister ymm1;
-    YMMRegister ymm2;
-    YMMRegister ymm3;
-    YMMRegister ymm4;
-    YMMRegister ymm5;
-    YMMRegister ymm6;
-    YMMRegister ymm7;
-    YMMRegister ymm8;
-    YMMRegister ymm9;
-    YMMRegister ymm10;
-    YMMRegister ymm11;
-    YMMRegister ymm12;
-    YMMRegister ymm13;
-    YMMRegister ymm14;
-    YMMRegister ymm15;
-
-    uint64_t r15;
-    uint64_t r14;
-    uint64_t r13;
-    uint64_t r12;
-    uint64_t r11;
-    uint64_t r10;
-    uint64_t r9;
-    uint64_t r8;
-    uint64_t rbp;
-    uint64_t rdi;
-    uint64_t rsi;
-    uint64_t rbx;
-    uint64_t rdx;
-    uint64_t rcx;
-    uint64_t rax;
-
-	uint64_t rflags;
-};
-#pragma pack(pop)
+using Omnihook::RegisterContext;
+using Omnihook::YMMRegister;
 
 class MidHook : public IHook {
 public:
